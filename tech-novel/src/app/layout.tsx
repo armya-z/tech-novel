06c0fabd2dd8a85eb14fa95dca6861045ebf4140
armya-z <armya.zk@gmail.com>
2024-04-05 15:30:12 +0330

    Add: mui add and style clean up

--- tech-novel/src/app/layout.tsx
+++ tech-novel/src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme/MuiTheme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppRouterCacheProvider options={{ key: "css", enableCssLayer: true }}>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
